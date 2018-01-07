2585b34fe7f26c2caa526fc62be8e5307d20f086
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2018-01-07 18:47:07 +0100

    add parameter create_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,33 @@
 # This class installs and cofigures policyd-weight
 # daemon.
 #
+# Parameters:
+# $package
+#   package to install, defaults to
+#   $policydweight::params::package
+# $package_ensure
+#   defaults to 'installed'
+# $create_resources
+#   a Hash of Hashes to create additional resources eg. to 
+#   create a postfix whitelist.
+#   Defaults to {} (do not create any additional resources)
+#   Example (hiera):
+# 
+#   policyweight::create_resources:
+#       postfix::map:
+#           mymap:
+#             source: '/tmp/test'
+#
+#    Will result in  executing:
+#
+#    postfix::map{'mymap':
+#      source => '/tmp/test',
+#    }
+#
 class policydweight (
   $package          = $policydweight::params::package,
   $package_ensure   = 'installed',
+  $create_resources = {},
 ) inherits policydweight::params {
 
 
@@ -14,4 +38,8 @@ class policydweight (
 
   include ::policydweight::service
   include ::policydweight::config
+
+  $create_resources.each | $res, $vals | {
+    create_resources($res, $vals)
+  }
 }
